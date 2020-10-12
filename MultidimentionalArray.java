670af7b4d7d282b9091d4a04ad5edabab3f47434
rizisadim-hub <72719715+rizisadim-hub@users.noreply.github.com>
2020-10-12 10:47:14 +0800

    Add files via upload

---
+++ MultidimentionalArray.java
@@ -0,0 +1,3 @@
+public class MultidimentionalArray {
+    int numbers[][] = {{1, 2, 3, 4, 5 }, {6, 7, 8, 9, 10}};
+}
---
+++ PrintArrays.java
@@ -0,0 +1,6 @@
+public class PrintArrays extends MultidimentionalArray {
+    public static void main(String[] args) {
+        MultidimentionalArray arrays = new MultidimentionalArray();
+        System.out.println(arrays.numbers[1][2]);
+    }
+}
