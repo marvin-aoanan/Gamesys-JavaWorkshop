f976797affd5dd1a0b3305eb27bdc05c70fa5a0a
rizisadim-hub <72719715+rizisadim-hub@users.noreply.github.com>
2020-10-14 11:14:48 +0800

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+
+
+    public static void main(String[] args) {
+        int radius = 10;
+        double pi = 3.14;
+        double c = 2 * pi * radius;
+        System.out.println(c);
+        System.out.println(pi * (radius * radius));
+    }
+}
