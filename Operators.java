4892f494e884bf79783864697ea1f4aeae090dea
marvin-aoanan <cybro.vin@gmail.com>
2020-10-14 16:06:24 +0800

    Add files via upload
    Updated Operators.java exercise

--- Operators.java
+++ Operators.java
@@ -1,11 +1,18 @@
 public class Operators {
-
-
     public static void main(String[] args) {
-        int radius = 10;
-        double pi = 3.14;
-        double c = 2 * pi * radius;
-        System.out.println(c);
-        System.out.println(pi * (radius * radius));
+        double Pi = 3.1416;
+        double Radius = 5;
+        double Celsius = 30;
+
+        double getCircumference = 2 * Pi * Radius;
+        double getArea = Pi * Radius * Radius;
+
+        double getFahrenheit = Celsius * 1.8 + 32;
+        double getKelvin = Celsius + 273.15;
+
+        System.out.println("Circumference: " + getCircumference);
+        System.out.println("Area: " + getArea);
+        System.out.println("Fahrenheit: " + getFahrenheit);
+        System.out.println("Kelvin: " + getKelvin);
     }
 }
