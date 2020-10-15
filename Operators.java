59b9055797a585c10193e26308f3c2185f86bece
marvin-aoanan <cybro.vin@gmail.com>
2020-10-15 10:20:42 +0800

    Update Operators.java
    Changed variable names to small letter / camelCase format

--- Operators.java
+++ Operators.java
@@ -1,14 +1,14 @@
 public class Operators {
     public static void main(String[] args) {
-        double Pi = 3.1416;
-        double Radius = 5;
-        double Celsius = 30;
+        double pi = 3.1416;
+        double radius = 5;
+        double celsius = 30;
 
-        double getCircumference = 2 * Pi * Radius;
-        double getArea = Pi * Radius * Radius;
+        double getCircumference = 2 * pi * radius;
+        double getArea = pi * radius * radius;
 
-        double getFahrenheit = Celsius * 1.8 + 32;
-        double getKelvin = Celsius + 273.15;
+        double getFahrenheit = celsius * 1.8 + 32;
+        double getKelvin = celsius + 273.15;
 
         System.out.println("Circumference: " + getCircumference);
         System.out.println("Area: " + getArea);
